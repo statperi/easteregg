d509f650f3f98d218dde73c165b06da2eed2a6f8
Periklis <periklis.stathopoulos@voicesage.com>
2022-04-05 10:04:50 +0100

    test coordinates

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,12 @@ window.onload = () => {
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (currentLocation) {
-            var data = {
-                latitude: currentLocation.coords.latitude,
-                longitude: currentLocation.coords.longitude
-            };
+            //var data = {
+            //    latitude: currentLocation.coords.latitude,
+            //    longitude: currentLocation.coords.longitude
+            //};
+
+            var data = { latitude: 53.3014227, longitude: -6.1777447 };
 
             let model = Model();
             model.location = data;
@@ -23,17 +25,9 @@ var Model = () => {
         url: './assets/easter_egg_2019/scene.gltf',
         rotation: '0 0 0',
         scale: '25 25 25',
-        positionText: '5 0 -5',
-        position: {
-            x: 5,
-            y: 0,
-            z: -5
-        },
+        position: '0 0 0',
         gestureConfig: 'minScale: 0.5; maxScale: 10',
-        info: 'Easter Egg',
-        text: null,
-        ground: false,
-        successAnimation: ''
+        info: 'Easter Egg'
     }
 }
 
@@ -42,37 +36,13 @@ function createEntity(model, autoscale) {
 
     let entityEl = createEntityElement(model);
     scene.appendChild(entityEl);
-
-    // test
-    $(entityEl)[0].setAttribute('position', model.positionText);
-    console.log('position:', model.positionText);
-
-    if (model.ground) {
-        let planeEl = createPlaneElement();
-        scene.appendChild(planeEl);
-    }
-
-    if (model.text) {
-        let textEl = createTextElement(model.text);
-        entityEl.appendChild(textEl);
-        // refresh(entityEl, textEl, autoscale);
-    }
 }
 
 function createEntityElement(config) {
     let element = document.createElement('a-entity');
     element.setAttribute('scale', config.scale);
     //element.setAttribute('rotation', config.rotation);
-
-    if (config.positionText) {
-        element.setAttribute('position', config.positionText);
-    }
-
-    if (config.position) {
-        element.object3D.position.x = config.position.x;
-        element.object3D.position.y = config.position.y;
-        element.object3D.position.z = config.position.z;
-    }
+    element.setAttribute('position', config.position);
 
     element.setAttribute('gltf-model', config.url);
     element.setAttribute('info', config.info);
@@ -80,48 +50,10 @@ function createEntityElement(config) {
     element.setAttribute('success', 'false');
     element.setAttribute('gps-entity-place', `latitude: ${config.location.latitude}; longitude: ${config.location.longitude};`);
 
-    if (config.lookAt == '[camera]') {
-        element.setAttribute('look-at', '[camera]');
-    }
-
     else if (config.gestureConfig) {
         element.setAttribute('gesture-handler', config.gestureConfig);
         element.classList.add('clickable');
     }
-
-    return element;
-}
-
-function createPlaneElement() {
-    let element = document.createElement('a-plane');
-    element.setAttribute('position', '0 0 0');
-    element.setAttribute('rotation', '-90 0 0');
-    element.setAttribute('width', '50');
-    element.setAttribute('height', '50');
-    element.setAttribute('material', 'shader: shadow');
-    element.setAttribute('shadow', '');
-    return element;
-}
-
-function createTextElement(config) {
-    let element = document.createElement('a-text');
-    element.setAttribute('value', config.text);
-    element.setAttribute('scale', config.scale)
-    element.setAttribute('look-at', '[gps-camera]');
-    element.setAttribute('gps-entity-place', `latitude: ${config.location.latitude}; longitude: ${config.location.longitude};`);
-
-    if (config.color) {
-        element.setAttribute('color', config.color);
-    }
-
-    if (config.lookAt == '[camera]') {
-        element.setAttribute('look-at', '[camera]');
-    }
-    else if (config.gestureConfig) {
-        element.setAttribute('gesture-handler', 'minScale: 0.5; maxScale: 10');
-        element.classList.add('clickable');
-    }
-    
 
     return element;
 }
